e1ce4965ae4ca66c12a0d05857442904ae684de5
Yannick Bättig <yannick.baettig@gmail.com>
2023-01-14 22:17:49 +0100

    added link on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { Inter } from "@next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className="max-w-lg mx-auto">Home</main>;
+  return (
+    <main className="max-w-lg mx-auto">
+      <Link href="/camera">Camera</Link>
+    </main>
+  );
 }
